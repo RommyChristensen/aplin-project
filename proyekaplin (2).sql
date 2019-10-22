37332f5a0d934157884b5cfc35e77aa89c14dc74
RommyChristensen <32613677+RommyChristensen@users.noreply.github.com>
2019-10-22 10:05:20 +0700

    Create proyekaplin (2).sql

---
+++ proyekaplin (2).sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.7
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 22, 2019 at 03:04 AM
+-- Server version: 5.6.37
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `proyekaplin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE IF NOT EXISTS `event` (
+  `event_id` int(11) NOT NULL,
+  `event_judul` varchar(50) NOT NULL,
+  `event_tgl` varchar(50) NOT NULL,
+  `event_desc` text NOT NULL,
+  `event_lokasi` varchar(50) NOT NULL,
+  `event_img` varchar(100) NOT NULL,
+  `event_tags` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `event`
+--
+
+INSERT INTO `event` (`event_id`, `event_judul`, `event_tgl`, `event_desc`, `event_lokasi`, `event_img`, `event_tags`) VALUES
+(1, 'barbeque 3', '10/29/2019', 'barbeque with pd istts', 'audit sby', '../events-poster/148064.jpg', ''),
+(2, 'barbeque 2', '10/29/2019', 'barbeque with pd istts', 'audit sby', '../events-poster/148064.jpg', ''),
+(3, 'barbeque 2', '10/29/2019', 'barbeque with pd istts', 'audit sby', '../events-poster/148064.jpg', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `Id` int(3) NOT NULL,
+  `Nama` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Username` varchar(30) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
